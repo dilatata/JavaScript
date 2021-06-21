8c0fe55d3d2784f15c50b42f4fca9a1943efbebe
khk <jasun1208@naver.com>
2021-06-21 16:30:12 +0900

    dto 수정

--- 0621_miniproject/dto.py
+++ 0621_miniproject/dto.py
@@ -1,34 +1,33 @@
-class CustDTO:
-    def __init__(self, newcustid, newemail, newname, newaddress, newphoneno, newpw):
-        self.custid = newcustid
+class DogOwnderDTO:
+
+    def __init__(self, newownerid, newemail, newownername, newaddress, newphoneno, newpasswoed):
+        self.ownerid = newownerid
         self.email = newemail
-        self.name = newname
+        self.ownername = newownername
         self.address = newaddress
         self.phoneno = newphoneno
-        self.pw = newpw
-    
-    def getCustid(self):
-        return self.custid
+        self.password = newpasswoed
+
+    def getOwnerid(self):
+        return self.ownerid
 
     def getEmail(self):
         return self.email
 
-    def getName(self):
-        return self.name
-
-    def getAddress(self):
-        return self.address
+    def getOwnername(self):
+        return self.ownername
 
     def getPhoneno(self):
         return self.phoneno
 
-    def getPw(self):
-        return self.pw
-    
+    def getPassword(self):
+        return self.password
+
     def __str__(self):
-        return 'Id : ' + self.custid + '- Email : ' + self.email + '- name: ' + self.name + - '- address' + self.address + '- Phone : ' + self.phoneno + '- password : ' + self.pw
+        return 'Id : ' + self.ownerid + '- Email : ' + self.email + '- owner name : ' + self.ownername + '- adress : ' + self.address + '- Phoneno : ' + self.phoneno + '- password : ' + self.password
     
-    
+
+
 
 class RoomDTO:
     def __init__(self, newroomno, newroomname, newroomtype, newroomdesc):
@@ -50,50 +49,112 @@ class RoomDTO:
         return self.newroomdesc
 
     def __str__(self):
-        return 'room no: ' + self.roomno + '- room name : ' + self.roomname + '- room type : ' + self.roomtype + '- room desc : ' + self.roomdesc
+        return 'room no : ' + self.roomno + '- room name : ' + self.roomname + '- room type : ' + self.roomtype + '- room desc : ' + self.roomdesc
 
 
 
 
-
-        
 class BookDTO:
-    def __init__(self, newbookingid, newcustid, newroomno, newbdate, newchindate, newchoutdate, newcancel, newprice):
+    def __init__(self, newbookingid, newownerid, newroomno, newbookingdate, newchindate, newchoutdate, newcancel,newdogname, newdogsize, newdogbreed ,newprice):
         self.bookingid = newbookingid
-        self.custid = newcustid
+        self.ownerid = newownerid
         self.roomno = newroomno
-        self.bdate = newbdate
-        self.chindate = newchindate
-        self.choutdate = newchoutdate
-        self.cancel = newcancel
-        self.price = newprice
+        self.bookingdate = newbookingdate
+        self.checkindate = newchindate
+        self.checkoutdate = newchoutdate
+        self.cancellation = newcancel
+        self.dogname = newdogname
+        self.dogsize = newdogsize
+        self.dogbreed = newdogbreed
+        self.totalprice = newprice
 
     def getBookingid(self):
         return self.bookingid
 
-    def getCustid(self):
-        return self.custid
+    def getOwnerid(self):
+        return self.ownerid
 
     def Roomno(self):
         return self.roomno
 
-    def getBdate(self):
-        return self.bdate
+    def getBookingdate(self):
+        return self.bookingdate
 
-    def getChindate(self):
-        return self.chindate
+    def getCheckindate(self):
+        return self.checkindate
 
-    def getChoutdate(self):
-        return self.choutdate
+    def getCheckoutdate(self):
+        return self.checkoutdate    
 
-    def getCancle(self):
-        return self.cancel
+    def getCancellation(self):
+        return self.cancellation
     
-    def getPrice(self):
-        return self.price
+    def getDogname(self):
+        return self.dogname
+    
+    def getDogsize(self):
+        return self.dogsize
+    
+    def getDogBreed(self):
+        return self.dogbreed
+        
+    def getTotalprice(self):
+        return self.totalprice
 
     def __str__(self):
-        return 'booking id: ' + self.bookingid + '- room custid : ' + self.custid + '- room no : ' + self.roomno + '- bookingdate : ' + self.bdate + '- check in date: ' + self.chindate + '- check out date : ' + self.choutdate + '- cancelation : ' + self.cancel + '- Total Price : ' + self.cancel
+        return 'booking id: ' + self.bookingid + '- room custid : ' + self.ownerid + '- room no : ' + self.roomno + '- bookingdate : ' + self.bookingdate + '- check in date: ' + self.checkindate + '- check out date : ' + self.checkoutdate + '- cancelation : ' + self.cancellation \
+            + '- dog name : ' + self.dogname + '- dog size : ' + self.dogsize + '- dog breed : ' + self.dogbreed + '- Total Price : ' + self.totalprice
 
 
+class DogDTO:
+
+    def __init__(self, newdogname, newownername, newdogsize, newdogbreed):
+        
+        self.dogname = newdogname
+        self.ownername = newownername
+        self.dogsize = newdogsize
+        self.dogbreed = newdogbreed
+
+    def getDogname(self):
+        return self.dogname
     
+    def getOwnername(self):
+        return self.ownername
+
+    def getDogsize(self):
+        return self.dogsize
+
+    def getDogbreed(self):
+        return self.dogbreed
+
+    def __str__(self):
+        return 'dog name : ' + self.dogname + '- owner name : ' + self.ownername + '- dog size : ' + self.dogsize + '- dog breed : ' + self.dogbreed
+
+
+class RoomTypeDTO:
+    def __init__(self, newroomtype, newlength, newwidth, newheight, newroomtypedesc):
+
+        self.roomtype = newroomtype
+        self.length = newlength
+        self.width = newwidth
+        self.height = newheight
+        self.roomtypedesc = newroomtypedesc
+
+    def getRoomtype(self):
+        return self.roomtype
+
+    def getLength(self):
+        return self.length
+    
+    def getWidth(self):
+        return self.width
+    
+    def getHeight(self):
+        return self.height
+    
+    def getRoomtypedesc(self):
+        return self.roomtypedesc
+
+    def __str__(self):
+        return 'room type : ' + self.roomtype + '- length : ' + self.length + '- width : ' + self.width + '- height : ' + self.height + ' - room type desc : ' + self.roomtypedesc
+
