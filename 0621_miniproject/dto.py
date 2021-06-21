593cc63ab84707c7c2642283c4a2a4a0c1898331
khk <jasun1208@naver.com>
2021-06-21 15:21:50 +0900

    RoomDTO 클래스 추가

--- 0621_miniproject/dto.py
+++ 0621_miniproject/dto.py
@@ -24,3 +24,30 @@ class CustDTO:
 
     def getPw(self):
         return self.pw
+    
+    def __str__(self):
+        return 'Id : ' + self.custid + '- Email : ' + self.name + '- Phone : ' + self.phone + '- password : ' + self.pw
+    
+    
+
+class RoomDTO:
+    def __init__(self, newroomno, newroomname, newroomtype, newroomdesc):
+        self.roomno = newroomno
+        self.roomname = newroomname
+        self.roomtype = newroomtype
+        self.roomdesc = newroomdesc
+
+    def getRoomno(self):
+        return self.newroomno
+
+    def getRoomname(self):
+        return self.newroomname
+
+    def getRoomtype(self):
+        return self.newroomtype
+
+    def getRoomdesc(self):
+        return self.newroomdesc
+
+    def __str__(self):
+        return '방 번호 : ' + self.roomno + '- 방 이름 : ' + self.roomname + '- 방 종류 : ' + self.roomtype + '- 방 설명 : ' + self.roomdesc
