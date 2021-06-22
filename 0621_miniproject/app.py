1f9e07a29c6ac70833f99bc6b671f64454e0a8ff
khk <jasun1208@naver.com>
2021-06-22 16:14:21 +0900

    로그인 jwt db연동 x

--- 0621_miniproject/app.py
+++ 0621_miniproject/app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template, request
+from logging import DEBUG
+from flask import Flask, request, render_template, jsonify
 from flask.signals import request_finished
+from flask_jwt_extended import *
+import datetime
 from dao import *
 from dto import *
 
+
+
 app = Flask(__name__)
+
+
 
 @app.route('/', methods=["GET"])
 def home():
@@ -26,9 +33,43 @@ def join():
 
 
 
-@app.route('/login', methods=["POST"])
+
+# 여기서부터 jwt로그인
+app.config.update(
+    DEBUG=True,
+    JWT_SECRET_KEY="I'M IML"
+)
+
+jwt = JWTManager(app)
+
+admin_id = "tester"
+admin_pw = "11"
+
+
+@app.route('/login', methods=['GET'])
 def login():
-    pass
+    return render_template("login.html")
+
+
+@app.route('/login', methods=['POST'])
+def login_proc():
+    user_id = request.form.get("id")
+    user_pw = request.form.get("pw")
+
+    if (user_id == admin_id and user_pw == admin_pw):
+        return jsonify(result=200, access_token=create_access_token(identity=user_id))
+    else:
+        return jsonify(result="Invalid Params!")
+
+
+@app.route("/jwtconfirm")
+@jwt_required()   # 참조 사이트에선 () 표현이 누락, 즉 버전에 따른 차이
+def jwt_confirm():
+    cur_user = get_jwt_identity()
+    print(cur_user)
+    return '{"id":"' + cur_user + '"}'
+
+
 
 
 if __name__ == "__main__":
