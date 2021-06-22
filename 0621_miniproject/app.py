7f5ef65f164b515c192f2755f20b2e4d4087feaa
khk <jasun1208@naver.com>
2021-06-22 14:22:40 +0900

    회원가입 완료

--- 0621_miniproject/app.py
+++ 0621_miniproject/app.py
@@ -13,33 +13,18 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/join', methods=["GET","POST"])
+
+
+@app.route('/join', methods=['GET','POST'])
 def join():
-    if request.method == 'GET':
-        return render_template('join.html')
+    
+    dao = InfoDAO()
+    dto = DogOwnerDTO(request.form.get("ownerid"), request.form.get("oewnername"), request.form.get("password"), request.form.get("email"), request.form.get("address"), request.form.get("phoneno"), request.form.get("accumpoints"))
+    dao.insertjoin(dto)
 
-    else:
-        pass
-		# ownerid = request.form.get('ownerid')
-		# ownername = request.form.get('ownername')
-		# password = request.form.get('password')
-		# email = request.form.get('email')
-        # address = request.form.get('address')
-		# phoneno = request.form.get('phoneno')
+    return render_template('join.html')
 
-		# if not (userid and username and password and re_password):
-		# 	return "모두 입력해주세요"
-		# elif password != re_password:
-		# 	return "비밀번호를 확인해주세요"
-		# else:
-		# 	user = User()
-		# 	user.password = password
-		# 	user.userid = userid
-		# 	user.username = username
-		# 	db.session.add(user)
-		# 	db.session.commit()
-		# 	return "회원가입 완료"
-		# return redirect('/')
+
 
 @app.route('/login', methods=["POST"])
 def login():
--- 0621_miniproject/dao.py
+++ 0621_miniproject/dao.py
@@ -1,3 +1,4 @@
+from logging import exception
 import cx_Oracle
 from dto import *
 
@@ -7,7 +8,7 @@ class InfoDAO:
 
 
 
-    def join(self, dto):
+    def insertjoin(self, dto):
         conn = cx_Oracle.connect(user="gurune", password="jungguru", dsn="xe")
         cur = conn.cursor()
         print("-------test1")
@@ -23,13 +24,41 @@ class InfoDAO:
             cur.close()
             conn.close()
 
+    # def selectone(self, ownerid):
+
+    #     data = ''
+    #     try:
+    #         conn = cx_Oracle.connect(user="gurune", password="jungguru", dsn="xe")
+    #         cur = conn.cursor()
+
+    #         try:
+    #             cur.execute("select * from dogowner where ownerid :=ownerid", ownerid=ownerid)
+    #             row = cur.fetchone()
+    #             data = '{"email":"' + row[1] + '", "ownername":"' + row[2] + '", "password":"' + row[3] + '", "address":"' + row[4] + '", "phoneno":' + str(row[5]) + ' "accumpoints":' + str(row[6]) + '}'
+
+    #         except Exception as e:
+    #             print(e)
+
+    #     except Exception as e:
+    #         print(e)
+
+    #     finally:
+    #         cur.close()
+    #         conn.close()
+
+    #     return data
+
+
+
+
+
 
     def login(self, dto):
         data = ''
         try:
             conn = cx_Oracle.connect(user="gurune", password="jungguru", dsn="xe")
             cur = conn.cursor()
-
+            print("-----test")
             try:
                 cur.execute("select ownerid, password from dogowner where ownerid=:ownerid and password=:password", \
                     owenrid=dto.getOwnerid(), password=dto.getPassword()) 
@@ -51,10 +80,11 @@ class InfoDAO:
     
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    dao = InfoDAO()
-    dto = DogOwnerDTO('s12un1234', 'jasun112208@nave2sar.com', 'jaes2eo21n','a4s22d', 'qw2e123', 7937212801, 45)
-    # dao.join(dto)
-    # dto = DogOwnerDTO('sun1234', 'a4s2d')
-    # dao.login(dto)
+#     dao = InfoDAO()
+#     # dto = DogOwnerDTO('s12un1234', 'jasun112208@nave2sar.com', 'jaes2eo21n','a4s22d', 'qw2e123', 7937212801, 45)
+#     # dao.join(dto)
+#     # dto = DogOwnerDTO('sun1234')
+#     dao.selectone('sun1234')
+#     # dao.login(dto)
