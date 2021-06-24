eb22a84fca199324d92a125c01d8d90571a68ae9
khk <jasun1208@naver.com>
2021-06-24 10:33:13 +0900

    예약정보 화면 수정 및 가입화면 수정

--- 0621_miniproject/dao.py
+++ 0621_miniproject/dao.py
@@ -53,33 +53,7 @@ class InfoDAO:
 
         return data
 
-# ---------------------------------------------------------------------
 
-    def login1(self, dto):
-        data = ''
-        try:
-            conn = cx_Oracle.connect(
-                user="gurune", password="jungguru", dsn="xe")
-            cur = conn.cursor()
-            try:
-                cur.execute("select owenerid, password from dogowner where ownerid=:ownerid and password=:password",
-                            ownerid=dto.getOwnerid(), password=dto.getPassword())
-                row = cur.fetchone()
-                print("--------")
-                data = '{"password":' + '"'+row[0]+'"}'
-                data = row[1]
-
-            except Exception as e:
-                print(e)
-
-        except Exception as e:
-            print(e)
-
-        finally:
-            cur.close()
-            conn.close()
-
-        return data
 
 
 
@@ -110,38 +84,35 @@ class InfoDAO:
 
 
 
-
     def selectinfo(slef, ownerid):
         data = []  
         try:
             conn = cx_Oracle.connect(user="gurune", password="jungguru", dsn="xe")
             cur = conn.cursor()
             try:
-                cur.execute("select * from booking where ownerid=:ownerid and cancellation=0", ownerid=ownerid) 
+                cur.execute("select rownum, bookingid, ownerid, roomno, bookingdate, checkindate, checkoutdate, cancellation, dogname, dogsize, dogbreed, totalprice from \
+                    (select * from booking where ownerid=:ownerid and cancellation=0 order by checkindate asc)", ownerid=ownerid)
+
                 rows = cur.fetchall() 
-                # json 포멧으로 가공 : empno/ename/sal key - json 배열
-                # 다양한 방법 : 저장하는 순서를 유지하는 구조의 dict 클래스 
-                v = []   # v 변수에 python구조의 dict 구조로 저장 -> data 변수로 json 포멧으로 변환
+                v = []
                 for row in rows:
-                    # print(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10])
 
                     d = collections.OrderedDict()
-                    d["bookingid"] = row[0]
-                    d["ownerid"] = row[1]
-                    d["roomno"] = row[2]
-                    d["bookingdate"] = row[3].strftime('%Y-%m-%d')
-                    d["checkindate"] = row[4].strftime('%Y-%m-%d')
-                    d["ckeckoutdate"] = row[5].strftime('%Y-%m-%d')
-                    d["cancellation"] = row[6]
-                    d["dogname"] = row[7]
-                    d["dogsize"] = row[8]
-                    d["dogbreed"] = row[9]
-                    d["totalprice"] = row[10]
-                    v.append(d)   # 이미 존재하는 list의 마지막 요소로 저장(add)
-                    # print(rows)
+                    d["rownum"] = row[0]
+                    d["bookingid"] = row[1]
+                    d["ownerid"] = row[2]
+                    d["roomno"] = row[3]
+                    d["bookingdate"] = row[4].strftime('%Y-%m-%d')
+                    d["checkindate"] = row[5].strftime('%Y-%m-%d')
+                    d["ckeckoutdate"] = row[6].strftime('%Y-%m-%d')
+                    d["cancellation"] = row[7]
+                    d["dogname"] = row[8]
+                    d["dogsize"] = row[9]
+                    d["dogbreed"] = row[10]
+                    d["totalprice"] = row[11]
+                    v.append(d)
 
-                data = json.dumps(v, sort_keys=True, default=str, ensure_ascii=False)   # json 포멧으로 자동 변환
-                print(data)
+                data = json.dumps(v, sort_keys=True, default=str, ensure_ascii=False)
 
             except Exception as e:
                 print(e) 
@@ -155,10 +126,10 @@ class InfoDAO:
 
 
 
-# if __name__ == "__main__":
-#     dao = InfoDAO()
+if __name__ == "__main__":
+    dao = InfoDAO()
 #     # dto = DogOwnerDTO('abc', 'abc@naver.com', 'juyoung', '1234', 'youngin', 8212312345)
 #     # dao.login(dto)
 #     dto = BookDTO(0, 'cdf' , 1, '21/03/21', '21/04/12', '21/04/13', 0, 'bonggu', 7, 'shiba', 60000)
 #     dao.insertbook2(dto)
-
+    dao.selectinfo("qwe")
